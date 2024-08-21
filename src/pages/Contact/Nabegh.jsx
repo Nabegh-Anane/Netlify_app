373e27150fe0714e31c7fc6b34c95f5082434cd7
Nabegh ANANE <contact@nabeghanane.com>
2024-08-21 11:26:50 +0100

    resolve link of HAckerRank

--- src/pages/Contact/Nabegh.jsx
+++ src/pages/Contact/Nabegh.jsx
@@ -20,7 +20,7 @@ const Nabegh = () => {
 							<a href="https://twitter.com/AnaneNabegh" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/twitter.png")} alt="Twitter icon" /></a>
 							<a href="https://www.behance.net/anenenebagh" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/behance.png")} alt="Behance icon" /></a>
 							<a href="https://codepen.io/nabegh-e" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/codepen.png")} alt="CodePen icon" /></a>
-							<a href="https://www.hackerrank.com/anenenebagh" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/hackerrank.png")} alt="HackerRank icon" /></a>
+							<a href="https://www.hackerrank.com/profile/anenenebagh" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/hackerrank.png")} alt="HackerRank icon" /></a>
 							<a href="https://t.me/Nevo_07" target="_blank" rel="noreferrer"> <img src={getImageUrl("contact/telegram.png")} alt="Telegram icon" /></a>
 							<a href="https://wa.me/+21699733961" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/whatsapp.png")} alt="Whatsapp icon" /></a>
               <a href="https://dribbble.com/nabegh07" target="_blank" rel="noreferrer"><img src={getImageUrl("contact/dribbble.png")} alt="Dribbble icon" /></a>
